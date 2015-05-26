a6e8647114aad57051bfd4eb3d8aea4456a3fa42
Angus Gratton <gus@projectgus.com>
2015-05-26 10:24:54 +1000

    Clean up FreeRTOSConfig defaults, increase min task stack size

--- examples/simple/FreeRTOSConfig.h
+++ examples/simple/FreeRTOSConfig.h
@@ -123,19 +123,5 @@ to exclude the API function. */
 #define INCLUDE_xTaskGetCurrentTaskHandle 1
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
 
-/* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
-(lowest) to 0 (1?) (highest). */
-#define configKERNEL_INTERRUPT_PRIORITY 		255
-/* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
-See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	191 /* equivalent to 0xb0, or priority 11. */
-
-
-/* This is the value being used as per the ST library which permits 16
-priority values, 0 to 15.  This must correspond to the
-configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
-NVIC value of 255. */
-#define configLIBRARY_KERNEL_INTERRUPT_PRIORITY	15
-
 #endif /* FREERTOS_CONFIG_H */
 
