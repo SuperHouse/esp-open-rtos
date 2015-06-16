b7b16de429c5ed536e6a62c05221a0c5a750117d
Angus Gratton <gus@projectgus.com>
2015-06-16 16:22:55 +1000

    Alias ets_printf directly to printf at link time
    I thought maybe ets_printf had some special properties, but it doesn't
    seem to have any so far.

--- core/sdk_compat.c
+++ core/sdk_compat.c
@@ -13,16 +13,3 @@ void *zalloc(size_t nbytes)
 {
     return calloc(1, nbytes);
 }
-
-/* this is currently just a stub to see where in the SDK it gets
-   called, and with what arguments...
-
-   In the binary SDK printf & ets_printf are aliased together, most
-   references appear to be to printf but libphy, libwpa & libnet80211
-   all call ets_printf sometimes... Seems to not be in common code
-   paths, haven't investigated exactly where.
-*/
-int ets_printf(const char *format, ...)  {
-    return printf("ets_printf format=%s\r\n", format);
-}
-
