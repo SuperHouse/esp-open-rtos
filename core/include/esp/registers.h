177ad281aa21905e5ccb63fccab6df206d40222c
Alex Stewart <Alexander.Stewart@consensuscorp.com>
2015-08-19 16:48:11 -0700

    Add esp/wdt_regs.h

--- core/include/esp/registers.h
+++ core/include/esp/registers.h
@@ -19,10 +19,8 @@
 #include "esp/iomux_regs.h"
 #include "esp/gpio_regs.h"
 #include "esp/timer_regs.h"
+#include "esp/wdt_regs.h"
 #include "esp/dport_regs.h"
-
-/* Internal macro, only defined in header body */
-#define _REG(BASE, OFFSET) (*(esp_reg_t)((BASE)+(OFFSET)))
 
 /* Register base addresses
 
@@ -38,19 +36,11 @@
 //#define TIMER_BASE (MMIO_BASE + 0x0600)
 #define RTC_BASE   (MMIO_BASE + 0x0700)
 //#define IOMUX_BASE (MMIO_BASE + 0x0800)
-#define WDT_BASE   (MMIO_BASE + 0x0900)
+//#define WDT_BASE   (MMIO_BASE + 0x0900)
 #define I2C_BASE   (MMIO_BASE + 0x0d00)
 #define UART1_BASE (MMIO_BASE + 0x0F00)
 #define RTCB_BASE  (MMIO_BASE + 0x1000)
 #define RTCS_BASE  (MMIO_BASE + 0x1100)
 #define RTCU_BASE  (MMIO_BASE + 0x1200)
-
-/* WDT register(s)
-
-   Not fully understood yet. Writing 0 here disables wdt.
-
-   See ROM functions esp_wdt_xxx
- */
-#define WDT_CTRL       _REG(WDT_BASE, 0x00)
 
 #endif
