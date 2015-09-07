1221e2117b8c13a8f753cf79ff1725de0b6e78ed
Angus Gratton <gus@projectgus.com>
2015-09-08 09:59:59 +1000

    Rework component makefiles to have a default <component>_ROOT
    This fixes a long-standing bug where build directories sometimes
    expanded many levels deeper than they should have.
    
    Component names can now no longer contains slashes (ie the component
    name for 'extras/i2c' is now just 'i2c')

--- extras/bmp180/component.mk
+++ extras/bmp180/component.mk
@@ -1,9 +1,9 @@
 # Component makefile for extras/bmp180
 
-INC_DIRS += $(ROOT)extras
+# expected anyone using bmp driver includes it as 'bmp180/bmp180.h'
+INC_DIRS += $(bmp180_ROOT)..
 
 # args for passing into compile rule generation
-extras/bmp180_INC_DIR =  $(ROOT)extras
-extras/bmp180_SRC_DIR =  $(ROOT)extras/bmp180
+bmp180_SRC_DIR =  $(bmp180_ROOT)
 
-$(eval $(call component_compile_rules,extras/bmp180))
+$(eval $(call component_compile_rules,bmp180))
