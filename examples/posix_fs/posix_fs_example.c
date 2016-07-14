1db953e0c3e096f9e7ff442f6c50dece002df6e1
sheinz <shein@bk.ru>
2016-07-15 00:08:34 +0300

    SPIFFS: Add speed test.

--- examples/posix_fs/posix_fs_example.c
+++ examples/posix_fs/posix_fs_example.c
@@ -1,5 +1,6 @@
 #include "espressif/esp_common.h"
 #include "esp/uart.h"
+#include "esp/timer.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "esp8266.h"
@@ -10,6 +11,10 @@
 
 #include "fs-test/fs_test.h"
 
+static fs_time_t get_current_time()
+{
+     return timer_get_count(FRC2) / 5000;  // to get roughly 1ms resolution
+}
 
 void test_task(void *pvParameters)
 {
@@ -24,9 +29,17 @@ void test_task(void *pvParameters)
 
     while (1) {
         vTaskDelay(5000 / portTICK_RATE_MS);
-
-        if (fs_test_run(1000)) {
+        if (fs_load_test_run(100)) {
             printf("PASS\n");
+        } else {
+            printf("FAIL\n");
+        }
+
+        vTaskDelay(5000 / portTICK_RATE_MS);
+        float write_rate, read_rate;
+        if (fs_speed_test_run(get_current_time, &write_rate, &read_rate)) {
+            printf("Read speed: %.0f bytes/s\n", read_rate * 1000); 
+            printf("Write speed: %.0f bytes/s\n", write_rate * 1000); 
         } else {
             printf("FAIL\n");
         }
