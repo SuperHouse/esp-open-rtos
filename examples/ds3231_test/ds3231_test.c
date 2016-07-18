f388bbc7c47c9442463b7512e2380dbc508bbb54
Bhuvanchandra <bhuvanchandra.dv@gmail.com>
2016-07-18 23:14:23 +0000

    examples: ds3231: Add simple example for ds3231
    Simple example for reading out the time and temperature
    from ds3231 RTC every second.
    
    Signed-off-by: Bhuvanchandra DV <bhuvanchandra.dv@gmail.com>

---
+++ examples/ds3231_test/ds3231_test.c
@@ -0,0 +1,41 @@
+/* Test code for DS3231 high precision RTC module 
+ *
+ * Part of esp-open-rtos
+ * Copyright (C) 2016 Bhuvanchandra DV <bhuvanchandra.dv@gmail.com>
+ * MIT Licensed as described in the file LICENSE
+ */
+#include "espressif/esp_common.h"
+#include "esp/uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "i2c/i2c.h"
+
+#include "ds3231/ds3231.h"
+
+void task1(void *pvParameters)
+{
+    struct tm time;
+    float tempFloat;
+
+    while(1) {
+        vTaskDelay(100);
+        ds3231_getTime(&time);
+	ds3231_getTempFloat(&tempFloat);
+        printf("TIME:%d:%d:%d, TEMPERATURE:%.2f DegC\r\n", time.tm_hour, time.tm_min, time.tm_sec, tempFloat);
+    }
+}
+
+void user_init(void)
+{
+    const int scl = 5, sda = 4;
+
+    uart_set_baud(0, 115200);
+
+    printf("\n");
+    printf("SDK version : %s\n", sdk_system_get_sdk_version());
+    printf("GIT version : %s\n", GITSHORTREV);
+
+    ds3231_Init(scl, sda);
+
+    xTaskCreate(task1, (signed char *)"tsk1", 256, NULL, 2, NULL);
+}
