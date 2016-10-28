5a14ab31e49efa5acbc234acd3962bd1af01586e
Ruslan V. Uss <UncleRus@users.noreply.github.com>
2016-10-28 18:29:47 +0600

    DS18x20: DS18S20 support, bugfixes (#255)

--- extras/ds18b20/component.mk
+++ extras/ds18b20/component.mk
@@ -6,4 +6,10 @@ INC_DIRS += $(ds18b20_ROOT)..
 # args for passing into compile rule generation
 ds18b20_SRC_DIR =  $(ds18b20_ROOT)
 
+# users can override this setting and get console debug output
+DS18B20_DEBUG ?= 0
+ifeq ($(DS18B20_DEBUG),1)
+	ds18b20_CFLAGS = $(CFLAGS) -DDS18B20_DEBUG
+endif
+
 $(eval $(call component_compile_rules,ds18b20))
