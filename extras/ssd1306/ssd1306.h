ac429f91484af9a34b04b778f8e81dee4e27a60a
lilian <lilian.marazano@gmail.com>
2016-12-07 08:15:36 +0100

    SH1106 port initial.

--- extras/ssd1306/ssd1306.h
+++ extras/ssd1306/ssd1306.h
@@ -29,6 +29,17 @@ extern "C"
 #endif
 
 /**
+ * SH1106 pump voltage value
+ */
+typedef enum
+{
+    SH1106_VOLTAGE_74 = 0, // 7.4 Volt
+    SH1106_VOLTAGE_80,     // 8.0 Volt
+    SH1106_VOLTAGE_84,     // 8.4 Volt
+    SH1106_VOLTAGE_90      // 9.0 Volt
+} sh1106_voltage_t;
+
+/**
  * I/O protocols
  */
 typedef enum
@@ -39,11 +50,21 @@ typedef enum
 } ssd1306_protocol_t;
 
 /**
+ * Screen type
+ */
+typedef enum
+{
+    SSD1306_SCREEN = 0,
+    SH1106_SCREEN
+} ssd1306_screen_t;
+
+/**
  * Device descriptor
  */
 typedef struct
 {
     ssd1306_protocol_t protocol;
+    ssd1306_screen_t screen ;
     union {
 #if (SSD1306_I2C_SUPPORT)
         uint8_t addr ;          //!< I2C address, used by SSD1306_PROTO_I2C
@@ -162,6 +183,14 @@ int ssd1306_set_display_start_line(const ssd1306_t *dev, uint8_t start);
  * @return Non-zero if error occured
  */
 int ssd1306_set_display_offset(const ssd1306_t *dev, uint8_t offset);
+
+/**
+ * Select charge pump voltage. See value in datasheet.
+ * @param dev Pointer to device descriptor
+ * @param select Select charge pump voltage value
+ * @return Non-zero if error occured
+ */
+int sh1106_set_charge_pump_voltage(const ssd1306_t *dev, sh1106_voltage_t select);
 
 /**
  * Enable or disable the charge pump. See application note in datasheet.
